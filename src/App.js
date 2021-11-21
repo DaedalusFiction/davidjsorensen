380b07a6859310c04489291ad5101b13723a4150
David Sorensen <teardownthismall@gmail.com>
2021-11-21 14:50:00 -0500

    updated favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import daedalusLogo from './images/logo.png'
 function App() {
   return (
-    <div >
-      Hello
+    <div className="flex justify-between text-right text-4xl mx-auto p-3 max-w-screen-xl text-gray-800 font-bold ">
+    <a href="#"><img className="" src={daedalusLogo} alt="Nautilus shell" /></a>
+      <div className="">David J Sorensen</div>
     </div>
   );
 }
